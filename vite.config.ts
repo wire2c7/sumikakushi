a06a9be017613db880a07bade94847020fc81ba4
wire2c7 <51051751+wire2c7@users.noreply.github.com>
2025-09-20 00:55:57 +0900

    tsconfig.jsonとvite.config.tsを修正
    ## 修正理由
    
    `App.tsx`で`ts(7026)`が出たため。
    
    ## 修正点
    
    - `tsconfig.json`
      - `jsxImportSource`を変更
    - `vite.config.ts`
      - `esbuild.jsxImportSource`を追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { cloudflare } from '@cloudflare/vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  esbuild: {
+    jsxImportSource: 'hono/jsx/dom',
+  },
   plugins: [cloudflare()],
 });
